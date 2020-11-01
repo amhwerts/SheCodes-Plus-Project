e2fabdd5258daffecbbd14e4d0437b97ee1d2e26
Amanda W <amhwerts@gmail.com>
2020-11-01 08:55:37 -0600

    Updated Time and weather description
    Updated the time stamp when loading new city and CSS for weather description.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,25 @@
 
             axios.get(apiUrl).then(showTemperature);
         }
-
+function formatDate(timestamp){
+let date=new Date(timestamp);
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDay = days[date.getDay()];
+let hours=date.getHours();
+let minutes=date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+return `as of ${currentDay}, ${hours}:${minutes}`;
+}
         function showTemperature(response) {
             console.log(response.data);
             console.log(response.data.main.temp);
@@ -18,10 +36,12 @@
             let humidityElement=document.querySelector("#humidity");
             let windElement=document.querySelector("#windSpeed");
             let temperatureElement = document.querySelector("#temperature");
+            let timeElement=document.querySelector("#time");
             temperatureElement.innerHTML = `${temperature}`;
             descriptionElement.innerHTML = response.data.weather[0].description;
             humidityElement.innerHTML = response.data.main.humidity;
             windElement.innerHTML=Math.round(response.data.wind.speed);
+            timeElement.innerHTML=formatDate(response.data.dt*1000);
 
         }
 
@@ -36,7 +56,7 @@
                 let units = "imperial";
                 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
-                axios.get(apiUrl).then(showTemperature) (updateTime);
+                axios.get(apiUrl).then(showTemperature);
 
             }
         function getCurrentPosition(){
@@ -58,11 +78,11 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let currentDay = days[currentTime.getDay()];
+let currentDay = days[timestamp.getDay()];
 let currentHour = currentTime.getHours();
 let currentMinute = currentTime.getMinutes();
 if (currentMinute < 10) {
   currentMinute = `0${currentMinute}`;
 }
 let date = document.querySelector("#time");
-date.innerHTML = `as of ${currentDay} ${currentHour}:${currentMinute}`;
+date.innerHTML = `${currentDay} ${currentHour}:${currentMinute}`;

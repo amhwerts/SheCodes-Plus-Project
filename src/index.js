915c5db1e597a80dd9bf35d38e6b0e902b3adfa8
Amanda W <amhwerts@gmail.com>
2020-11-01 08:40:14 -0600

    Updated multiple weather aspects in current weather.
    added Humidity, Weather Description, Wind Speed, and updating current time. Also adjusted IDs for 5day forecast.

--- src/index.js
+++ src/index.js
@@ -9,24 +9,34 @@
 
             axios.get(apiUrl).then(showTemperature);
         }
+
         function showTemperature(response) {
             console.log(response.data);
             console.log(response.data.main.temp);
             let temperature = Math.round(response.data.main.temp);
+            let descriptionElement= document.querySelector("#descriptionWeather");
+            let humidityElement=document.querySelector("#humidity");
+            let windElement=document.querySelector("#windSpeed");
             let temperatureElement = document.querySelector("#temperature");
             temperatureElement.innerHTML = `${temperature}`;
+            descriptionElement.innerHTML = response.data.weather[0].description;
+            humidityElement.innerHTML = response.data.main.humidity;
+            windElement.innerHTML=Math.round(response.data.wind.speed);
+
         }
+
+
         function showPosition(position) {
                 console.log(position);
                 let lat = position.coords.latitude;
                 let lon = position.coords.longitude;
                 let h2 = document.querySelector("h2");
-                h2.innerHTML = `Current Weather in Current Location`;
+                h2.innerHTML = `Weather in Current Location`;
                 let apiKey = "da530f5aaed37a0a9bea3a612727e5a6";
                 let units = "imperial";
                 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
-                axios.get(apiUrl).then(showTemperature);
+                axios.get(apiUrl).then(showTemperature) (updateTime);
 
             }
         function getCurrentPosition(){
@@ -38,3 +48,21 @@
         let currentLocationButton =document.querySelector("#current-location-button");
         currentLocationButton.addEventListener("click", getCurrentPosition);
         
+        let currentTime = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDay = days[currentTime.getDay()];
+let currentHour = currentTime.getHours();
+let currentMinute = currentTime.getMinutes();
+if (currentMinute < 10) {
+  currentMinute = `0${currentMinute}`;
+}
+let date = document.querySelector("#time");
+date.innerHTML = `as of ${currentDay} ${currentHour}:${currentMinute}`;

f5f0dd1ccdbca37effbfc35ebb8ff88556ca1ef7
Amanda W <amhwerts@gmail.com>
2020-11-01 09:17:55 -0600

    Updated current weather Icon
    Updated the current weather icon to update when reloading new city information, including alt text.

--- src/index.js
+++ src/index.js
@@ -37,14 +37,15 @@ return `as of ${currentDay}, ${hours}:${minutes}`;
             let windElement=document.querySelector("#windSpeed");
             let temperatureElement = document.querySelector("#temperature");
             let timeElement=document.querySelector("#time");
+            let iconElement=document.querySelector("#currentIcon");
             temperatureElement.innerHTML = `${temperature}`;
             descriptionElement.innerHTML = response.data.weather[0].description;
             humidityElement.innerHTML = response.data.main.humidity;
             windElement.innerHTML=Math.round(response.data.wind.speed);
             timeElement.innerHTML=formatDate(response.data.dt*1000);
-
+            iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
         }
-
 
         function showPosition(position) {
                 console.log(position);
